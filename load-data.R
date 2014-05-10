1fd740d06ac9a2c6827978ce7340f39de4765654
Chris Dalzell <cdalzell@gmail.com>
2014-05-09 21:03:12 -0400

    Actually are going to need to merge these into a DateTime column

--- load-data.R
+++ load-data.R
@@ -1,8 +1,10 @@
 loadPowerData <- function() {
   dataFrame <- read.csv2("data\\household_power_consumption.txt")
   
-  dataFrame$Date <- strptime(dataFrame$Date, "%d/%m/%Y")
-  dataFrame <- subset(dataFrame, as.Date(Date) >= '2007-2-1' & as.Date(Date) < '2007-2-3')
+  dataFrame$DateTime <- strptime(paste(dataFrame$Date, dataFrame$Time), "%d/%m/%Y %H:%M:%S")
+
+  dataFrame <- subset(dataFrame, select = -c(Date, Time))
+  dataFrame <- subset(dataFrame, as.Date(DateTime) >= "2007-2-1" & as.Date(DateTime) < "2007-2-3")  
     
   return (dataFrame)
 }
